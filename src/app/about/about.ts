631baa280cc95c7afc727a3e33bc66107fa78dbd
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2025-06-21 12:08:08 +0300

    add loader

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.html',
-  styleUrl: './about.css'
+  styleUrl: './about.css',
 })
 export class About {
-
+  http = inject(HttpClient);
+  users: any[] = [];
+  isLoading = true;
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.http.get('/api/User/GetAllUsers').subscribe(
+      (res: any) => {
+        this.users = res.data;
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Error:', error);
+        this.isLoading = false;
+      }
+    );
+  }
 }
--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,5 +1,4 @@
 import { CommonModule, JsonPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -11,13 +10,6 @@ import { Router } from '@angular/router';
 })
 export class Dashboard {
   router = inject(Router);
-  http = inject(HttpClient);
-  users: any[] = [];
-  ngOnInit(): void {
-    this.http.get('api/User/GetAllUsers').subscribe((req: any) => {
-      this.users = req.data;
-    });
-  }
   logout() {
     localStorage.removeItem('token');
     this.router.navigate(['/']);
